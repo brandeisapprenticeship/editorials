233ec0fb6c96787b92c08a061c9c4a91f8a601bd
Maria Altebarmakian <m.altebarmakian@gmail.com>
2015-09-10 10:19:54 -0400

    added layout and fixed devise

--- editorials/app/models/user.rb
+++ editorials/app/models/user.rb
@@ -6,11 +6,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  protected
-  	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :name, :admin, :optout)}
-  		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me, :name)}
-  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name)}
-  	end
 end
