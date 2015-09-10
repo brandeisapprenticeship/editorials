9fb3abc5fe800eba9092d7227f358837025e4312
Maria Altebarmakian <m.altebarmakian@gmail.com>
2015-09-10 10:39:14 -0400

    added form for assignments and homepage view

--- editorials/db/schema.rb
+++ editorials/db/schema.rb
@@ -34,6 +34,22 @@ ActiveRecord::Schema.define(version: 20150910135714) do
     t.datetime "updated_at",                null: false
   end
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",    limit: 255, null: false
+    t.string   "data_content_type", limit: 255
+    t.integer  "data_file_size",    limit: 4
+    t.integer  "assetable_id",      limit: 4
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width",             limit: 4
+    t.integer  "height",            limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
   create_table "comments", force: :cascade do |t|
     t.text     "content",    limit: 65535
     t.integer  "user_id",    limit: 4
